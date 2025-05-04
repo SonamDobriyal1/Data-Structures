64eabdc2823709411f0c9206cdb97bed1108b5f2
Sonam Dobriyal <dobriyalsonam22@gmail.com>
2025-05-04 22:05:35 +0530

    Add files via upload

---
+++ sliding window/anagram.py
@@ -0,0 +1,14 @@
+x="appleppaapppal"
+find="appl"
+x=sorted(x)
+find=sorted(find)
+hash=[-1]*26
+#for i in range(len(x)):
+ #   arr[x[i]]+=1
+for i in range(len(x)):
+    l=hash[x[i]]+1
+    print(l)
+    
+    
+    
+    
